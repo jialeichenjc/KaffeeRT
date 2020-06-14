33e64d73e34e5f039185dc7591991978378d68b6
Joyce Chen <superbrainchen@gmail.com>
2020-06-13 21:51:25 -0700

    Integrate Google Test into project

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include "gtest/gtest.h"
 
-int main() {
+int testFunc() {
+    return 2;
+}
+
+TEST(testSuite1, test1) {
+    EXPECT_EQ(testFunc(), 2);
+    std::cout << "Test passed" << std::endl;
+}
+int main(int argc, char **argv) {
     std::cout << "Hello world!!" << std::endl;
+    ::testing::InitGoogleTest(&argc, argv);
+/*
+TEST(TestSuiteName, TestName) {
+  ... test body ...
+}
+*/
+    
 
-
-    return 0;
+    return RUN_ALL_TESTS();
 }
