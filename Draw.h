1a332d8d971583b855edcdb9c2502ab8c211f516
Joyce Chen <superbrainchen@gmail.com>
2020-06-18 11:06:27 -0700

    Add some canvas ops and tests

--- Draw.h
+++ Draw.h
@@ -12,8 +12,15 @@ public:
     Canvas(size_t i_width, size_t i_height) :
     width(i_width), height(i_height) {
         assert(width > 0 && height > 0);
-        pixels = new Vec4[width][height];
+    }
 
+    Canvas() {
+        pixels = new Vec4*[600];
+        for(int i = 0; i < 600; i++) {
+            pixels[i] = new Vec4[800];
+        }
+        width = 800;
+        height = 600;
     }
 
     ~Canvas() {
@@ -27,9 +34,11 @@ public:
 
 // x: width y: height
 void writeToPixel(Canvas &canvas, size_t x, size_t y, const Vec4 &color) {
-
+    assert(x < canvas.width && y < canvas.height);
+    canvas.pixels[x][y] = color;
 }
 
-Vec4 colorAtPixel(Canvas &canvas, size_t x, size_t y) {
-    return Vec4(0, 0, 0);
+Vec4 getColorAtPixel(Canvas &canvas, size_t x, size_t y) {
+    assert(x < canvas.width && y < canvas.height);
+    return canvas.pixels[x][y];
 }
--- TestDraw.h
+++ TestDraw.h
@@ -4,5 +4,9 @@
 #include "Draw.h"
 
 TEST(DrawTests, testCreateCanvas) {
-    Canvas canvas(800, 600);
+    Canvas canvas;
+    Vec4 color = getColorAtPixel(canvas, 250, 300);
+    EXPECT_EQ(canvas.width, 800);
+    EXPECT_EQ(canvas.height, 600);
+    EXPECT_EQ(color, Vec4(0.0f, 0.0f, 0.0f));
 }
