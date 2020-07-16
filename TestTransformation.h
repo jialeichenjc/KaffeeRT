5a52932a06d3a5a68d05606b8fc001619e17c1be
Joyce Chen <superbrainchen@gmail.com>
2020-07-16 16:07:13 +0800

    Finish translation test

--- TestTransformation.h
+++ TestTransformation.h
@@ -8,4 +8,11 @@ TEST(TransTests, testTranslation) {
     Vec4 p(-3, 4, 5, 1);
 
     EXPECT_EQ(t * p, Vec4(2, 1, 7, 1));
+
+    Mat4 inv = inverse(t);
+    EXPECT_EQ(inv * p, Vec4(-8, 7, 3, 1));
+
+    Vec4 v(-3, 4, 5);
+    EXPECT_EQ(t * v, v);
 }
+
